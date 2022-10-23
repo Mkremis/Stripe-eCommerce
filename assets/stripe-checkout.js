7213c539db5b60243a3b2da30322ac83a211ef58
Mkremis <105755654+Mkremis@users.noreply.github.com>
2022-10-23 14:18:12 -0300

    Add files via upload

---
+++ assets/stripe-checkout.js
@@ -0,0 +1,67 @@
+import stripeKeys from "./stripe-keys.js";
+import STRIPE_KEYS from "./stripe-keys.js";
+
+const $tacos = document.getElementById("tacos"),
+  $template = document.getElementById("taco-template").content,
+  $fragment = document.createDocumentFragment(),
+  fetchOptions = { headers: { Authorization: `Bearer ${STRIPE_KEYS.secret}` } },
+  moneyFormat = (num) => `$${num.slice(0, -2)},${num.slice(-2)}`;
+
+let products, prices;
+
+Promise.all([
+  fetch("https://api.stripe.com/v1/products", fetchOptions),
+  fetch("https://api.stripe.com/v1/prices", fetchOptions),
+])
+  .then((responses) => Promise.all(responses.map((res) => res.json())))
+  .then((json) => {
+    products = json[0].data;
+    prices = json[1].data;
+    console.log(products, prices);
+    prices.forEach((price) => {
+      let price_id = price.id,
+        price_amount = moneyFormat(price.unit_amount_decimal),
+        currency = price.currency.toUpperCase(),
+        product_id = price.product,
+        productData = products.filter((product) => product_id === product.id),
+        productImg = productData[0].images[0],
+        productName = productData[0].name,
+        $figure = $template.querySelector(".taco"),
+        $img = $template.querySelector(".taco img");
+      $figure.setAttribute("data-price", price_id);
+      $img.src = productImg;
+      $img.id = product_id;
+      $img.alt = productName;
+      $template.querySelector(
+        ".taco figcaption"
+      ).innerHTML = `${productName} <br> ${currency} ${price_amount}`;
+      let $clone = document.importNode($template, true);
+      $fragment.appendChild($clone);
+    });
+    $tacos.appendChild($fragment);
+  })
+  .catch((err) => {
+    console.log(err);
+    let message =
+      err.statusText || "Ocurrió un error al conectarse con la API de Stripe";
+    $tacos.innerHTML = `<p>Error ${err.status} : ${message}</p>`;
+  });
+
+document.addEventListener("click", (e) => {
+  if (e.target.matches(".taco *")) {
+    let price_id = e.target.parentElement.getAttribute("data-price");
+    Stripe(STRIPE_KEYS.public)
+      .redirectToCheckout({
+        lineItems: [{ price: price_id, quantity: 1 }],
+        mode: "payment",
+        successUrl: "http://127.0.0.1:5500/assets/stripe-success.html",
+        cancelUrl: "http://127.0.0.1:5500/assets/stripe-cancel.html",
+      })
+      .then((res) => {
+        if (res.error) {
+          console.log("respuesta", res);
+          $tacos.insertAdjacentElement("afterend", res.error.message);
+        }
+      });
+  }
+});
---
+++ assets/stripe-keys.js
@@ -0,0 +1,6 @@
+export default {
+  public:
+    "pk_test_51LZDwnDMRP0dsgwcwuNrKWO86AiNyd4ah7At3wOs7RBsSUEkMC1gpJkFrtHhSREbJ2gwz07BWCT4MlUJWvkyYD5H000FzBaBsR",
+  secret:
+    "sk_test_51LZDwnDMRP0dsgwcR7rT74I2r7u46cCFRCYyS3baECbinhyiKFr98VsQyTCE8JVJjb4tCnonVc8oHq2kMqKLdjRM00zo0l5z2O",
+};
